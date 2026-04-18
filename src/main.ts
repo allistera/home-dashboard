f374bba264357838bf7037c617ba052fee0e63c3
Allister Antosik <me@allisterantosik.com>
2026-04-18 12:45:54 +0100

    Enable sentry support

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueApexCharts from 'vue3-apexcharts'
+import * as Sentry from '@sentry/vue'
 
 const app = createApp(App)
+
+Sentry.init({
+  app,
+  dsn: 'https://a2c15284ce727c8aa289f8de0a906951@o4510748410576896.ingest.de.sentry.io/4511240658223184',
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+})
 
 app.use(router)
 app.use(VueApexCharts)
