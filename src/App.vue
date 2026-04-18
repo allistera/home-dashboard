8bb157468d8b5df53a361bbf9f985316768f2938
Allister Antosik <me@allisterantosik.com>
2026-04-18 11:15:42 +0100

    Enable Vercel Speed Insights

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <ThemeProvider>
     <SidebarProvider>
       <RouterView />
+      <SpeedInsights />
     </SidebarProvider>
   </ThemeProvider>
 </template>
 
 <script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue'
 import ThemeProvider from './components/layout/ThemeProvider.vue'
 import SidebarProvider from './components/layout/SidebarProvider.vue'
 </script>
