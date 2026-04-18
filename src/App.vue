53ccffa9fa49ab9637a958880240ff7d67cf8c8f
Allister Antosik <me@allisterantosik.com>
2026-04-18 12:14:53 +0100

    Install Vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <ThemeProvider>
     <SidebarProvider>
       <RouterView />
+      <Analytics />
       <SpeedInsights />
     </SidebarProvider>
   </ThemeProvider>
 </template>
 
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/vue'
 import { SpeedInsights } from '@vercel/speed-insights/vue'
 import ThemeProvider from './components/layout/ThemeProvider.vue'
 import SidebarProvider from './components/layout/SidebarProvider.vue'
